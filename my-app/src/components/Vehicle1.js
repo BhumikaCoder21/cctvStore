9bae78054fe8b8b88eef82dcc8903fd198a33d7c
BhumikaCoder21 <bhumikanew21@gmail.com>
2025-07-13 21:43:26 +0530

    contact

--- my-app/src/components/Vehicle1.js
+++ my-app/src/components/Vehicle1.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Services1.css";
+import ptzImage from "../assets/contactHead.webp";
 
 function Service1() {
   return (
     <div
       className="security-banner"
       style={{
-        backgroundImage: `url("https://aisite.10web.io/65/wp-content/uploads/sites/167/2025/06/tenweb_media_rhbcxhnxs.webp")`,
-      }}
+              backgroundImage: `url(${ptzImage})`,
+            }}
     >
       <div className="security-banner-content">
         <h1>Vehicle License Plate Reader CCTV Camera</h1>
